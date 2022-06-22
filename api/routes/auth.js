016afa196501b79af5538ee80385a3738044b6a3
Keshav <keshavjha018@gmail.com>
2022-06-22 15:05:53 +0530

    login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -23,4 +23,21 @@ router.post("/register", async(req,res)=>{
     }catch(e) {console.log(e)}
 })
 
+//Login
+router.post("/login", async(req,res)=>{
+    try {
+        //verify email
+        const user = await User.findOne({email:req.body.email});
+        !user && res.status(404).json("User not found !");
+
+        //verify pass
+        const validPass = await bcrypt.compare(req.body.password, user.password);
+        !validPass && res.status(400).json("Wrong Password !");
+
+        //if valid
+        res.status(200).json(user);
+
+    }catch(e) {console.log(e)}
+})
+
 module.exports = router;
