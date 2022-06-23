6f82323312e28aa597c99cf5fc68ceec83a1eb62
Keshav <keshavjha018@gmail.com>
2022-06-23 14:08:24 +0530

    Navbar done

---
+++ client/src/components/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Topbar from "../topbar/Topbar"
+
+function Home() {
+  return (
+    <>
+        <Topbar/>
+    </>
+  )
+}
+
+export default Home
---
+++ client/src/components/topbar/Topbar.js
@@ -0,0 +1,38 @@
+import "./Topbar.css"
+import {Search, Notifications, Person, Chat} from "@material-ui/icons"
+
+function Topbar() {
+  return (
+    <div className="topbarContainer">
+        <div className="topbarLeft">
+            <span className="logo">WeConnect</span>
+        </div>
+        <div className="topbarCenter">
+            <div className="searchBar">
+                <Search className="searchIcon"/>
+                <input placeholder='Search for Posts, Friends and more...' className="searchInput" />
+            </div>
+        </div>
+        <div className="topbarRight">
+            <div className="topbarLinks">
+                <span className="topbarLink">Homepage</span>
+                <span className="topbarLink">Timeline</span>
+            </div>
+            <div className="topbarIconItem">
+                <Notifications/>
+                <span className="topbarIconBadge">2</span>
+            </div>
+            <div className="topbarIconItem">
+                <Chat/>
+                <span className="topbarIconBadge">1</span>
+            </div>
+            <div className="topbarIconItem">
+                <Person/>
+            </div>
+            <img src="/assets/person/2.jpeg" alt="" className="topbarImg" />
+        </div>
+    </div>
+  )
+}
+
+export default Topbar
